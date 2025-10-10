e3858a1115dc92b56102fa43238c0f878a30ee3b
Amen Dhahri🌙 <invictus.dhahri@gmail.com>
2025-10-10 13:00:43 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ import { MoonGateProvider, MoongateConnectButton } from '@moongate/sdk';
 
 export default function Home() {
   return (
-    <MoonGateProvider apiKey={process.env.NEXT_PUBLIC_MOONGATE_API_KEY!} >
+    // apiKey={process.env.NEXT_PUBLIC_MOONGATE_API_KEY!}
+    <MoonGateProvider apiKey={'mg_pk_903dc0258b360f3eadeddc4a343d31f8'}  >
       <div className="min-h-screen flex items-center justify-center">
         <MoongateConnectButton />
       </div>
